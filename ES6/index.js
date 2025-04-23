33e38bae2b3ea8589b51f357c16f743b3cd6ecef
Alex-hushiyi <hu_shiyi@163.com>
2025-04-24 00:08:12 +0800

    ES6

---
+++ ES6/index.js
@@ -0,0 +1,8 @@
+// 数组解构
+var names = ["abc", "cba", "nba"]
+var [a, b, c] = names
+console.log(a) // abc
+// 对象解构
+var obj = { name: "abc", age: 18 }
+var { name, age } = obj
+console.log(name) // abc
--- 对象/类.js
+++ 对象/类.js
@@ -1,36 +1,64 @@
 class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-    sayHello() {
-        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
-    }
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  sayHello() {
+    console.log(
+      `Hello, my name is ${this.name} and I am ${this.age} years old.`
+    );
+  }
 
-    get info() {
-        return `${this.name}, ${this.age} years old`;
-    }
-    set info(value) {
-        const [name, age] = value.split(', ');
-        this.name = name;
-        this.age = parseInt(age);
-    }
-    static greet() {
-        console.log("Hello from the Person class!");
-    }
+  get info() {
+    return `${this.name}, ${this.age} years old`;
+  }
+  set info(value) {
+    const [name, age] = value.split(", ");
+    this.name = name;
+    this.age = parseInt(age);
+  }
+  static greet() {
+    console.log("Hello from the Person class!");
+  }
 }
 
 class Student extends Person {
-    constructor(name, age, studentId) {
-        super(name, age);
-        this.studentId = studentId;
-    }
-    study() {
-        console.log(`${this.name} is studying.`);
-    }
+  constructor(name, age, studentId) {
+    super(name, age);
+    this.studentId = studentId;
+  }
+  study() {
+    console.log(`${this.name} is studying.`);
+  }
 
-    sayHello() {
-        super.sayHello();
-        console.log(`My student ID is ${this.studentId}.`);
-    }
+  sayHello() {
+    super.sayHello();
+    console.log(`My student ID is ${this.studentId}.`);
+  }
 }
+
+// 继承内置类
+class ADate extends Date {
+  constructor(year, month, day) {
+    super(year, month, day);
+  }
+  getFormattedDate() {
+    return `${this.getFullYear()}-${this.getMonth() + 1}-${this.getDate()}`;
+  }
+}
+
+var AD = new ADate(2023, 10, 1);
+console.log(AD.getFormattedDate()); // 2023-11-1
+
+// 混入
+function mixinRunner(BaseClass) {
+  class NewClass extends BaseClass {
+    running() {
+      console.log("running~");
+    }
+  }
+  return NewClass;
+}
+
+var ns = mixinRunner(Student);
+ns.running(); // running~
